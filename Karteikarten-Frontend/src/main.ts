21cd3100db870a0297e85bb2a6feb9b290a033c6
leonderdunna <leonderdunna@gmail.com>
2022-03-05 17:32:10 +0100

    gleap hinzugefügt

--- Karteikarten-Frontend/src/main.ts
+++ Karteikarten-Frontend/src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+import Gleap from 'gleap';
+
+// Please make sure to call this method only once!
+Gleap.initialize("vx6iJW97SlJOMO1MKcLUAMElgPAxYtlo");
+
 if (environment.production) {
   enableProdMode();
 }
